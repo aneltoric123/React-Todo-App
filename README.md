324f52f43de28d99cbc7219fb98873fa8f6c871c
Anel Torić <aneltoric@Anels-MacBook-Air.local>
2024-07-02 10:20:25 +0200

    Opravila

--- README.md
+++ README.md
@@ -1,70 +1,44 @@
-# Getting Started with Create React App
+ # Aplikacija za Seznam Opravil (Todo List)
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+To je preprosta aplikacija za seznam opravil, zgrajena z uporabo React in komponent iz Carbon Design System.
 
-## Available Scripts
+## Funkcionalnosti
 
-In the project directory, you can run:
+- Dodajanje novih opravil
+- Označevanje opravil kot opravljena ali neopravljena
+- Urejanje obstoječih opravil
+- Brisanje opravil
+- Filtriranje opravil glede na status opravljenosti
+- Brisanje vseh dokončanih opravil
 
-### `npm start`
+## Namestitev
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Za zagon aplikacije lokalno sledite tem korakom:
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+1. Klonirajte repozitorij:
 
-### `npm test`
+   
+   git clone <repository-url>
+   cd todo-list-app
+2. Namestite odvisnosti:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+   npm install
+   npm install carbon-components carbon-components-react
 
-### `npm run build`
+3. Zaženite strežnik:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+  npm start
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+4. Odpri svoj brskalnik in obišči http://localhost:3000, da si ogledaš aplikacijo.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Slika aplikacije
 
-### `npm run eject`
+![Todo List Screenshot](public/todo-list-screenshot.png)
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+# Prilagodite poti <repository-url> v ukazih za kloniranje in namestitev glede na dejansko pot do vašega repozitorija. Prav tako v datoteko dodajte sliko (todo-list-screenshot.png) svoje aplikacije, da prikažete, kako izgleda v praksi.
+Ta datoteka README.md vam bo pomagala pri razumevanju namestitve, uporabe in funkcionalnosti vaše aplikacije za seznam opravil
 
-## Learn More
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/TodoList.js
+++ src/TodoList.js
@@ -5,6 +5,7 @@ import {
   Checkbox,
   ListItem,
   UnorderedList,
+  DatePicker,
 } from 'carbon-components-react';
 import './TodoList.css';
 
@@ -16,6 +17,8 @@ const TodoList = () => {
   const [newTodo, setNewTodo] = useState('');
   const [isEditing, setIsEditing] = useState(null);
   const [currentText, setCurrentText] = useState('');
+  const [dueDate, setDueDate] = useState(null);
+  const [showCompleted, setShowCompleted] = useState(true);
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
@@ -23,35 +26,58 @@ const TodoList = () => {
 
   const addTodo = () => {
     if (newTodo.trim() !== '') {
-      setTodos([...todos, { text: newTodo, completed: false }]);
+      setTodos([...todos, { id: Date.now(), text: newTodo, completed: false, dueDate }]);
       setNewTodo('');
+      setDueDate(null);
     }
   };
 
-  const toggleComplete = (index) => {
-    const updatedTodos = todos.map((todo, i) =>
-      i === index ? { ...todo, completed: !todo.completed } : todo
+  const toggleComplete = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
     );
     setTodos(updatedTodos);
   };
 
-  const deleteTodo = (index) => {
-    setTodos(todos.filter((_, i) => i !== index));
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-  const startEditing = (index) => {
-    setIsEditing(index);
-    setCurrentText(todos[index].text);
+  const startEditing = (id) => {
+    const todoToEdit = todos.find((todo) => todo.id === id);
+    setIsEditing(id);
+    setCurrentText(todoToEdit.text);
   };
 
-  const saveTodo = (index) => {
-    const updatedTodos = todos.map((todo, i) =>
-      i === index ? { ...todo, text: currentText } : todo
+  const saveTodo = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, text: currentText } : todo
     );
     setTodos(updatedTodos);
     setIsEditing(null);
     setCurrentText('');
   };
+
+  const handleDateChange = (date) => {
+    setDueDate(date);
+  };
+
+  const toggleShowCompleted = () => {
+    setShowCompleted(!showCompleted);
+  };
+
+  const clearCompletedTodos = () => {
+    const filteredTodos = todos.filter((todo) => !todo.completed);
+    setTodos(filteredTodos);
+  };
+
+  const sortedTodos = todos.slice().sort((a, b) => {
+    if (a.completed && !b.completed) return 1;
+    if (!a.completed && b.completed) return -1;
+    return 0;
+  });
+
+  const filteredTodos = showCompleted ? sortedTodos : todos.filter((todo) => !todo.completed);
 
   return (
     <div className="todo-container">
@@ -62,21 +88,36 @@ const TodoList = () => {
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
         />
+        <DatePicker
+          id="due-date"
+          datePickerType="single"
+          onChange={handleDateChange}
+          value={dueDate}
+          labelText="Rok izpolnitve"
+        />
         <Button onClick={addTodo} className="add-button">
-          Dodaj
+          Dodaj opravilo
+        </Button>
+      </div>
+      <div className="todo-actions">
+        <Button onClick={toggleShowCompleted} className="filter-button">
+          {showCompleted ? 'Skrij dokončana' : 'Prikaži dokončana'}
+        </Button>
+        <Button onClick={clearCompletedTodos} className="clear-button">
+          Počisti dokončana
         </Button>
       </div>
       <UnorderedList className="todo-list">
-        {todos.map((todo, index) => (
-          <ListItem key={index} className="todo-item">
-            {isEditing === index ? (
+        {filteredTodos.map((todo) => (
+          <ListItem key={todo.id} className="todo-item">
+            {isEditing === todo.id ? (
               <>
                 <TextInput
-                  id={`edit-input-${index}`}
+                  id={`edit-input-${todo.id}`}
                   value={currentText}
                   onChange={(e) => setCurrentText(e.target.value)}
                 />
-                <Button onClick={() => saveTodo(index)} className="save-button">
+                <Button onClick={() => saveTodo(todo.id)} className="save-button">
                   Shrani
                 </Button>
               </>
@@ -85,13 +126,16 @@ const TodoList = () => {
                 <Checkbox
                   labelText={todo.text}
                   checked={todo.completed}
-                  id={`checkbox-${index}`}
-                  onChange={() => toggleComplete(index)}
+                  id={`checkbox-${todo.id}`}
+                  onChange={() => toggleComplete(todo.id)}
                 />
-                <Button onClick={() => startEditing(index)} className="edit-button">
+                {todo.dueDate && (
+                  <span className="due-date">Rok: {new Date(todo.dueDate).toLocaleDateString()}</span>
+                )}
+                <Button onClick={() => startEditing(todo.id)} className="edit-button">
                   Uredi
                 </Button>
-                <Button onClick={() => deleteTodo(index)} className="delete-button">
+                <Button onClick={() => deleteTodo(todo.id)} className="delete-button">
                   Izbriši
                 </Button>
               </>
